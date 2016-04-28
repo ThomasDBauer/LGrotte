63dce5a04ff68118eb1cac2a2c694762f3ccf7ea
enricopopaj <enrico.popaj@googlemail.com>
2016-04-28 11:02:45 +0200

    # WARNING: head commit changed in the meantime
    enrico hat MerkzettelMapper erzeugt

---
+++ LGrotte/src/de/server/db/MerkzettelMapper.java
@@ -0,0 +1,14 @@
+package de.server.db;
+
+import java.util.Vector;
+
+public class MerkzettelMapper {
+		
+	MerkzettelMapper merkzettelMapper; 
+	
+	public MerkzettelMapper MerkzettelMapper() {
+		return merkzettelMapper;
+	}
+	
+	
+}
