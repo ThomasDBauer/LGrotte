f9e93b08adcf5de4323e125c4cd90989e7af2539
enricopopaj <enrico.popaj@googlemail.com>
2016-04-28 16:32:29 +0200

    KontaktsperreMapper geändert

--- LGrotte/src/de/server/db/KontaktsperreMapper.java
+++ LGrotte/src/de/server/db/KontaktsperreMapper.java
@@ -1,11 +1,39 @@
 package de.server.db;
 
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import de.shared.BO.Profil;
+
+
+
 public class KontaktsperreMapper {
 	
-	KontaktsperreMapper kontaktsperreMapper;
+	private static KontaktsperreMapper kontaktsperreMapper = null;
 	
-	public KontaktsperreMapper KontaktsperreMapper() {
+	protected KontaktsperreMapper() {
+		
+	}
+	
+	public static KontaktsperreMapper kontaktsperreMapper() {
+		if (kontaktsperreMapper == null) {
+			kontaktsperreMapper = new KontaktsperreMapper();
+		}
 		return kontaktsperreMapper;	
+	}
+	
+	public void createKontaktsperreTable() throws Exception {
+		Connection con = (Connection) DBConnection.connection();
+		PreparedStatement createKontaktsperre = (PreparedStatement) con.prepareStatement("CREATE TABLE IF NOT EXISTS Kontaktsperre(sperrendesProfil varchar(255), gesperrtesProfil varchar(255), PRIMARY KEY(sperrendesProfil, gesperrtesProfil))");
+		createKontaktsperre.execute();
+	}
+	
+	public void insertMerkzettel(Profil gesperrtesProfil) throws Exception {
+		Connection con = (Connection) DBConnection.connection();
+		PreparedStatement insertKontaktsperre = (PreparedStatement) con.prepareStatement("INSERT INTO Kontaktsperre(gesperrtesProfil) VALUES ('" + gesperrtesProfil + "')");
+		insertKontaktsperre.execute();
 	}
 	
 	
