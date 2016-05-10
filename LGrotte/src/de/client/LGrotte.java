51c99e91db6bb0861034ec515c23173aadd21bec
SedatAkar <sedatakar991@hotmail.com>
2016-05-10 17:24:58 +0200

    Buttons hinzugefügt und mit "navi-button" für css bearbeitbar gemacht.

--- LGrotte/src/de/client/LGrotte.java
+++ LGrotte/src/de/client/LGrotte.java
@@ -2,7 +2,10 @@ package de.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 import de.client.temp.SeedButton;
 
@@ -12,8 +15,27 @@ public class LGrotte implements EntryPoint {
 
 	public void onModuleLoad() {
 		
+		HorizontalPanel navPanel = new HorizontalPanel();
+		
 		//Der SeedButton aus dem temp-package
 		RootPanel.get("Inhalt_oben").add(new SeedButton());
+		RootPanel.get("Navi").add(navPanel);
+		
+		final Button findLove = new Button("Find Love");
+		findLove.setStylePrimaryName("navi-button");
+		navPanel.add(findLove);
+		
+		final Button profil = new Button("Profil");
+		profil.setStylePrimaryName("navi-button");
+		navPanel.add(profil);
+		
+		final Button merkliste = new Button("Merkliste");
+		merkliste.setStylePrimaryName("navi-button");
+		navPanel.add(merkliste);
+		
+		final Button impressum = new Button("Impressum");
+		impressum.setStylePrimaryName("navi-button");
+		navPanel.add(impressum);
 			
 	}
 }
