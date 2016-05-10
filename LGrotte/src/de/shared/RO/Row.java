be5519a18a63f24b6eb886afb3619ebe50f0cf03
SedatAkar <sedatakar991@hotmail.com>
2016-05-10 14:59:00 +0200

    Speicherplatz für Spalten erschaffen und getColumns-Methode zur Auslesung aller Spalten erstellt.

--- LGrotte/src/de/shared/RO/Row.java
+++ LGrotte/src/de/shared/RO/Row.java
@@ -1,5 +1,18 @@
 package de.shared.RO;
 
-public class Row {
+import java.io.Serializable;
+import java.util.Vector;
+
+public class Row implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	// Speicherplatz für die Spalten der Zeile in einem Vector
+	private Vector<Column> columns = new Vector<Column>();
+
+	// Auslesen aller Spalten
+	public Vector<Column> getColumns() {
+		return columns;
+	}
 
 }
