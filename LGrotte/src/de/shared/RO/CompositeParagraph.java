b6f9600932e225d7109020317d907b6c941f7ecd
SedatAkar <sedatakar991@hotmail.com>
2016-05-10 16:06:01 +0200

    SimpleParagraph-Klasse erstellt.

--- LGrotte/src/de/shared/RO/CompositeParagraph.java
+++ LGrotte/src/de/shared/RO/CompositeParagraph.java
@@ -2,6 +2,10 @@ package de.shared.RO;
 
 import java.io.Serializable;
 
+
+/*
+ * Stellt eine Menge einzelner Absätze dar.
+ */
 public class CompositeParagraph extends Paragraph implements Serializable{
 	
 	  private static final long serialVersionUID = 1L;
