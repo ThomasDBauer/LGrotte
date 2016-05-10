94443eff6f11e80216404a9a0dff22f57ec5019c
SedatAkar <sedatakar991@hotmail.com>
2016-05-10 15:13:05 +0200

    Column()-Konstruktor erstellt getValue() erstellt setValue() erstellt toString() erstellt

--- LGrotte/src/de/shared/RO/Column.java
+++ LGrotte/src/de/shared/RO/Column.java
@@ -1,5 +1,33 @@
 package de.shared.RO;
 
-public class Column {
+import java.io.Serializable;
 
+public class Column implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String value = "";
+
+	public Column() {
+	}
+
+	//erzwingt einen Spalteneintrag
+	public Column(String s) {
+		this.value = s;
+	}
+
+	//Auslesen des Spaltenwerts
+	public String getValue() {
+		return value;
+	}
+
+	//Aktuellen Spaltenwert überschreiben
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	//Column-Objekt in String umwandeln
+	public String toString() {
+		return this.value;
+	}
 }
