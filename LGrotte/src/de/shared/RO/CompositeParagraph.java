6993a674777fc3eb06a9ddc14f273934aafe5787
SedatAkar <sedatakar991@hotmail.com>
2016-05-10 16:00:53 +0200

    CompositeParagraph-Klasse erstellt

---
+++ LGrotte/src/de/shared/RO/CompositeParagraph.java
@@ -0,0 +1,9 @@
+package de.shared.RO;
+
+import java.io.Serializable;
+
+public class CompositeParagraph extends Paragraph implements Serializable{
+	
+	  private static final long serialVersionUID = 1L;
+
+}
