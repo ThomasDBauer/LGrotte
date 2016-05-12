ce08223c784d411599aacc442d372f0c85ba47dc
SedatAkar <sedatakar991@hotmail.com>
2016-05-12 19:51:21 +0200

    letzter commit fuer heute.. bei der ausgabe eines testprofils gibts aber noch immer einen fehler :(

--- LGrotte/src/de/client/gui/PartnervorschlaegeReport.java
+++ LGrotte/src/de/client/gui/PartnervorschlaegeReport.java
@@ -1,5 +1,60 @@
 package de.client.gui;
 
-public class PartnervorschlaegeReport {
-	
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+
+import de.client.ClientSideSettings;
+import de.shared.BO.Profil;
+
+public class PartnervorschlaegeReport extends HorizontalPanel {
+	private HorizontalPanel pvR = this;
+
+	public PartnervorschlaegeReport() {
+		try {
+			ClientSideSettings.getReportService().showProfilReport(1, new ReportCallback());
+		} catch (Exception e1) {
+			pvR.add(new Label(e1.toString() + " im Partnervorschlaege@Konstruktor"));
+			e1.printStackTrace();
+		}
+
+	}
+
+	private class ReportCallback implements AsyncCallback<Profil> {
+		public void onFailure(Throwable caught) {
+			pvR.add(new Label(caught.toString() + " im ReportCallback"));
+		}
+
+		public void onSuccess(Profil profil) {
+			pvR.add(new Label(profil.getFname()));
+			pvR.add(new Label(profil.getLname()));
+		}
+	}
+
+	// private class findLoveButtonClickHandler implements ClickHandler {
+	// public void onClick(ClickEvent e) {
+	// try {
+	// ClientSideSettings.getReportService().showProfilReport(1, new
+	// ReportCallback());
+	// } catch (Exception e1) {
+	// pvR.add(new Label(e1.toString()));
+	// e1.printStackTrace();
+	// }
+	//
+	// }
+	// }
+
+	// private class ReportCallback implements AsyncCallback<Profil> {
+	// public void onFailure(Throwable caught) {
+	// pvR.add(new Label(caught.toString()));
+	// }
+	//
+	// public void onSuccess(Profil profil) {
+	// pvR.add(new Label(profil.getFname()));
+	// pvR.add(new Label(profil.getLname()));
+	// }
+	// }
 }
