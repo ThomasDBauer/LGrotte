ab8b52dc68139ef461e273641a5ccc3e5b1a809e
Marko Deveric <md092@hdm-stuttgart.de>
2016-05-12 11:55:43 +0200

    AehnlichkeitMapper - delete() InfoMapper - delete () DB Connection - einheitlich mit leerem PW und unnoetiger Comment weg SuchprofilMapper - insert(), fehlende Sachen kommentiert

--- LGrotte/src/de/server/db/SuchprofilMapper.java
+++ LGrotte/src/de/server/db/SuchprofilMapper.java
@@ -1,10 +1,28 @@
 package de.server.db;
 
+import java.sql.PreparedStatement;
+
+import com.google.cloud.sql.jdbc.Connection;
+
+import de.shared.BO.Suchprofil;
+
 public class SuchprofilMapper {
-//	public void insertSuchprofil (Suchprofil sp){
-//	
-//}
-//
+	public void insertSuchprofil (Suchprofil sp) throws Exception{
+	
+		
+		/**
+		 * Unser Suchprofil benötigt noch..
+		 * - eine ID
+		 * - ein Geschlecht (für sp.getGeschlecht)
+		 */
+		Connection con = (Connection) DBConnection.connection();
+		PreparedStatement insertSuchprofil = (PreparedStatement)con.prepareStatement(
+				"INSERT INTO suchprofil(suchprofil_id, geschlecht, raucher, religion, geburtsdatum,"
+				+ "koerpergroesse, haarfarbe) VALUES ('"+sp.getGeburtsdatum()+"','"+sp.getKoerpergroesse()+"'.'"+sp.getHaarfarbe()+"','"+sp.getReligion()+"')");
+		insertSuchprofil.execute();
+		
+}
+
 //public void updateSuchprofil (Suchprofil sp){
 //	
 //}
