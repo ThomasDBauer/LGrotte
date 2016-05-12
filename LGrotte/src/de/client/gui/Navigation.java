e5de4db4238aae0112d91a3dfbec370726792ef5
LukasKircher <lukas.kircher94@gmail.com>
2016-05-12 16:32:12 +0200

    Navigation aus kopiert LGrotte und in Navigation GUI-Klasse eingefügt

--- LGrotte/src/de/client/gui/Navigation.java
+++ LGrotte/src/de/client/gui/Navigation.java
@@ -1,7 +1,53 @@
 package de.client.gui;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.RootPanel;
+
+import de.client.temp.SeedButton;
 
 public class Navigation extends HorizontalPanel{
 
+	private static Navigation navigation;
+
+	protected Navigation(){
+HorizontalPanel navPanel = new HorizontalPanel();
+		
+		//Der SeedButton aus dem temp-package
+		RootPanel.get("Inhalt_oben").add(new SeedButton());
+		RootPanel.get("Navi").add(navPanel);
+		
+		//Der FindLove-Button
+		final Button findLoveButton = new Button("Find Love");
+		findLoveButton.setStylePrimaryName("navi-button");
+		navPanel.add(findLoveButton);
+		
+		//Der Profil-Button
+		final Button profilButton = new Button("Mein Profil");
+		profilButton.setStylePrimaryName("navi-button");
+		navPanel.add(profilButton);
+		
+		//Der Merkliste-Button
+		final Button merklisteButton = new Button("Merkliste");
+		merklisteButton.setStylePrimaryName("navi-button");
+		navPanel.add(merklisteButton);
+		
+		//Der Impressum-Button
+		final Button impressumButton = new Button("Impressum");
+		impressumButton.setStylePrimaryName("navi-button");
+		navPanel.add(impressumButton);
+		
+		findLoveButton.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				
+				
+			}
+		});	
+	}
+	
+	
 }
