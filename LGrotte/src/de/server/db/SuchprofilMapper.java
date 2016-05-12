1c9b61864a25411079db9c8f6c7db7d0008c3b1d
Marko Deveric <marko.deveric@outlook.de>
2016-05-12 17:47:20 +0200

    SuchprofilMapper createSuchprofilTable() Ist nur grob, SQL Statement nochmal kontrollieren.

--- LGrotte/src/de/server/db/SuchprofilMapper.java
+++ LGrotte/src/de/server/db/SuchprofilMapper.java
@@ -7,18 +7,30 @@ import com.google.cloud.sql.jdbc.Connection;
 import de.shared.BO.Suchprofil;
 
 public class SuchprofilMapper {
+	
+	public void createSuchprofilTable () throws Exception {
+		Connection con = (Connection) DBConnection.connection();
+		
+		PreparedStatement createSuchprofil = (PreparedStatement) con
+				.prepareStatement("CREATE TABLE IF NOT EXISTS suchprofil,"
+						+"suchprofil_id INT NOT NULL AUTO_INCREMENT,"
+						+"PRIMARY KEY(suchprofil_id),"
+						+"geschlecht boolean(1),"
+						+"geburtsdatum Date(1),"
+						+"religion varchar(255),"
+						+"raucher boolean(1)");
+		
+		createSuchprofil.execute();
+		
+	}
+	
+	
 	public void insertSuchprofil (Suchprofil sp) throws Exception{
 	
-		
-		/**
-		 * Unser Suchprofil benötigt noch..
-		 * - eine ID
-		 * - ein Geschlecht (für sp.getGeschlecht)
-		 */
 		Connection con = (Connection) DBConnection.connection();
 		PreparedStatement insertSuchprofil = (PreparedStatement)con.prepareStatement(
 				"INSERT INTO suchprofil(suchprofil_id, geschlecht, raucher, religion, geburtsdatum,"
-				+ "koerpergroesse, haarfarbe) VALUES ('"+sp.getGeburtsdatum()+"','"+sp.getKoerpergroesse()+"'.'"+sp.getHaarfarbe()+"','"+sp.getReligion()+"')");
+				+ "koerpergroesse, haarfarbe) VALUES ('"+sp.getId()+"','"+sp.isGeschlecht()+"'.'"+sp.getGeburtsdatum()+"','"+sp.getReligion()+"','"+sp.getKoerpergroesse()+"','"+sp.isRaucher()+"')");
 		insertSuchprofil.execute();
 		
 }
