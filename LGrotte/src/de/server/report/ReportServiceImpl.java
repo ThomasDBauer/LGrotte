66bec73c2bff05f6ccf954a0717a347a16623b5b
SedatAkar <sedatakar991@hotmail.com>
2016-05-12 17:49:57 +0200

    Profil showProfilReport() erstellt.

--- LGrotte/src/de/server/report/ReportServiceImpl.java
+++ LGrotte/src/de/server/report/ReportServiceImpl.java
@@ -3,13 +3,22 @@ package de.server.report;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
+import de.server.db.ProfilMapper;
 import de.server.db.Seeder;
 import de.shared.EditorService;
 import de.shared.ReportService;
+import de.shared.BO.Profil;
 
 /**
  * The server-side implementation of the RPC service.
  */
 @SuppressWarnings("serial")
-public class ReportServiceImpl extends RemoteServiceServlet implements ReportService{
+public class ReportServiceImpl extends RemoteServiceServlet implements ReportService {
+
+	Profil showProfilReport(int id) throws Exception {
+
+		Profil p = ProfilMapper.profilMapper().getProfilByID(id);
+		return p;
+	}
+
 }
