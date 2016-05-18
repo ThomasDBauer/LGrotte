b02f110406c84e838b4835b409178d8af14703ee
Thomas Bauer <tb094@hdm-stuttgart.de>
2016-05-18 17:01:41 +0200

    UserLogin Kleinkram

--- LGrotte/src/de/client/LGrotte.java
+++ LGrotte/src/de/client/LGrotte.java
@@ -13,6 +13,7 @@ import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 import de.client.gui.Editor;
+import de.client.gui.LoginFrame;
 import de.client.gui.Navigation;
 import de.client.temp.SeedButton;
 import de.client.temp.UserLoginTestGUI;
@@ -20,36 +21,8 @@ import de.shared.BO.Profil;
 
 public class LGrotte implements EntryPoint {
 
-	private final TestServiceAsync greetingService = GWT.create(TestService.class);
-	private VerticalPanel loginPanel = new VerticalPanel();
-	private Label loginLabel = new Label(
-			"Please sign in to your Google Account to access the StockWatcher application.");
-	private Anchor signInLink = new Anchor("Sign In");
-	final Anchor logOutLink = new Anchor("Logout");
-
 	public void onModuleLoad() {
 		
-		RootPanel.get("Einstellungen_oben").add(new SeedButton());
-		RootPanel.get("Navi").add(new Navigation());
-		RootPanel.get("Inhalt_unten").add(new Editor());
-		RootPanel.get("Einstellungen_unten").add(new UserLoginTestGUI());
-		
-
-		
-		ClientSideSettings.getLoginService().login(GWT.getHostPageBaseURL() + 
-				"LGrotte.html"
-				,new AsyncCallback<Profil>(){
-			public void onFailure(Throwable caught) {
-			}
-			public void onSuccess(Profil result) {
-				if(result.isLoggedIn()){
-				}else{
-				signInLink.setHref(result.getLoginUrl());
-				loginPanel.add(loginLabel);
-				loginPanel.add(signInLink);
-				RootPanel.get("Einstellungen_oben").add(loginPanel);
-			}}
-				
-		});
+		RootPanel.get().add(new LoginFrame());
 	}
 }
