ab907e1ca87a21c174a38903d42b25804e61f562
Thomas Bauer <tb094@hdm-stuttgart.de>
2016-05-18 13:32:04 +0200

    auf sedats stand gebracht..

--- LGrotte/src/de/client/gui/PartnervorschlaegeReport.java
+++ LGrotte/src/de/client/gui/PartnervorschlaegeReport.java
@@ -1,21 +1,20 @@
 package de.client.gui;
 
-
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.VerticalPanel;
 
 import de.client.ClientSideSettings;
 import de.shared.BO.Profil;
 
 public class PartnervorschlaegeReport extends HorizontalPanel {
 	private HorizontalPanel pvR = this;
-	private VerticalPanel vPanel = new VerticalPanel();
+
 
 	public PartnervorschlaegeReport() {
+
 		try {
-			ClientSideSettings.getReportService().showProfilReport("Thomas@LG", new ReportCallback());
+			ClientSideSettings.getReportService().showProfilReport(1, new ReportCallback());
 		} catch (Exception e1) {
 			pvR.add(new Label(e1.toString()));
 			e1.printStackTrace();
@@ -29,13 +28,12 @@ public class PartnervorschlaegeReport extends HorizontalPanel {
 		}
 
 		public void onSuccess(Profil profil) {
-			pvR.add(new Label(profil.getFname() + " " + profil.getLname()));
-			pvR.add(new Label(profil.getGeschlecht()));
-			pvR.add(new Label(profil.getHaarfarbe()));
-			pvR.add(new Label(profil.getReligion()));
-			pvR.add(new Label(profil.getRaucher()));
-			
+			 pvR.add(new Label(profil.getFname() + " " + profil.getLname() + " " + profil.getGeschlecht() + " "
+			 + profil.getHaarfarbe() + " " + profil.getReligion() + " " +
+			 profil.getRaucher()));
 
+
+			
 		}
 	}
 
