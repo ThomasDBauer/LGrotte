c7e3f9c4c3d85043a0e20356b86911401d68d3d2
SedatAkar <sedatakar991@hotmail.com>
2016-05-19 12:05:36 +0200

    Methoden in Klasse Report angepasst.

--- LGrotte/src/de/shared/RO/Report.java
+++ LGrotte/src/de/shared/RO/Report.java
@@ -7,16 +7,6 @@ public abstract class Report implements Serializable {
 	
 	 private static final long serialVersionUID = 1L;
 	
-	 private Paragraph imprint = null;
-
-	  /**
-	   * Kopfdaten des Berichts.
-	   */
-	  private Paragraph headerData = null;
-
-	  /**
-	   * Jeder Bericht kann einen individuellen Titel besitzen.
-	   */
 	  private String title = "Report";
 
 	  /**
@@ -24,43 +14,7 @@ public abstract class Report implements Serializable {
 	   */
 	  private Date created = new Date();
 
-	  /**
-	   * Auslesen des Impressums.
-	   * 
-	   * @return Text des Impressums
-	   */
-	  public Paragraph getImprint() {
-	    return this.imprint;
-	  }
-
-	  /**
-	   * Setzen des Impressums.
-	   * 
-	   * @param imprint Text des Impressums
-	   */
-	  public void setImprint(Paragraph imprint) {
-	    this.imprint = imprint;
-	  }
-
-	  /**
-	   * Auslesen der Kopfdaten.
-	   * 
-	   * @return Text der Kopfdaten.
-	   */
-	  public Paragraph getHeaderData() {
-	    return this.headerData;
-	  }
-
-	  /**
-	   * Setzen der Kopfdaten.
-	   * 
-	   * @param headerData Text der Kopfdaten.
-	   */
-	  public void setHeaderData(Paragraph headerData) {
-	    this.headerData = headerData;
-	  }
-
-	  /**
+	   /**
 	   * Auslesen des Berichtstitels.
 	   * 
 	   * @return Titeltext
