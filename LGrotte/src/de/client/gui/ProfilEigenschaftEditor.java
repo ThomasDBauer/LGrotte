61cbbfabfc2c6002ed6edbda64c03ee9b58c1cc9
NicolaiEhrmanntraut <Nicolai@10.60.57.110>
2016-05-20 13:40:34 +0200

    ProfilEigenschaftEditor erweitert

--- LGrotte/src/de/client/gui/ProfilEigenschaftEditor.java
+++ LGrotte/src/de/client/gui/ProfilEigenschaftEditor.java
@@ -1,14 +1,28 @@
 package de.client.gui;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class ProfilEigenschaftEditor extends VerticalPanel {
 
 	private VerticalPanel panel = this;
 	
+	private HorizontalPanel eigenschaftPanel = new HorizontalPanel();
+	private HorizontalPanel infoPanel = new HorizontalPanel();
+	
 	private Label ProfilErweiternLabel = new Label("Profil erweitern");
+	
+	private TextBox eigenschaftTextBox = new TextBox();
+	private TextBox InfoTextBox = new TextBox();
+	
+	private Button addEigenschaftButton = new Button("Eigenschaft hinzufügen");
+	private Button addInfoButton = new Button("Info hinzufügen");
 	
 	private ListBox auswahlEigenschaftBox = new ListBox();
 	
@@ -17,7 +31,16 @@ public class ProfilEigenschaftEditor extends VerticalPanel {
 		auswahlEigenschaftBox.addItem("hihi");
 		
 		this.add(ProfilErweiternLabel);
-		this.add(auswahlEigenschaftBox);
+		this.add(eigenschaftPanel);
+		eigenschaftPanel.add(eigenschaftTextBox);
+		eigenschaftPanel.add(addEigenschaftButton);
+		this.add(infoPanel);
+		infoPanel.add(auswahlEigenschaftBox);
+		infoPanel.add(InfoTextBox);
+		infoPanel.add(addInfoButton);
+		
 		
 	}
+	
+	
 }
