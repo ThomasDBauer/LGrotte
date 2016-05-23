870b7b90576d4714a1fdfb2833e99374807638bc
SedatAkar <sedatakar991@hotmail.com>
2016-05-23 12:05:20 +0200

    Klasse ausgebaut um Profil anhand LogInMail anzuzeigen. (Funktioniert noch nicht)

--- LGrotte/src/de/shared/RO/MeinProfilReport.java
+++ LGrotte/src/de/shared/RO/MeinProfilReport.java
@@ -1,5 +1,39 @@
 package de.shared.RO;
 
-public class MeinProfilReport {
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+
+import de.client.ClientSideSettings;
+import de.shared.BO.Profil;
+
+public class MeinProfilReport extends HorizontalPanel{
+	private HorizontalPanel pvR = this;
+	
+	public MeinProfilReport() {
+
+		Profil profil = new Profil();
+		
+		try {
+			ClientSideSettings.getReportService().showMyProfile(profil.getEmail(), new ReportCallback());
+		} catch (Exception e1) {
+			pvR.add(new Label(e1.toString()));
+			e1.printStackTrace();
+		}
+
+	}
+
+	private class ReportCallback implements AsyncCallback<String> {
+		public void onFailure(Throwable caught) {
+			pvR.add(new Label(caught.toString()));
+		}
+		//Er aber, sag's ihm, er kann mich im Arsche lecken!
+		public void onSuccess(String s) {
+			pvR.add(new HTML(s));
+			 
+			
+		}
+	}
 
 }
