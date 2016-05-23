46a3b28b2d212423944ec440cd320b3d621de329
NicolaiEhrmanntraut <Nicolai@10.60.41.102>
2016-05-23 16:29:12 +0200

    halleluja

--- LGrotte/src/de/client/gui/ProfilEigenschaftEditor.java
+++ LGrotte/src/de/client/gui/ProfilEigenschaftEditor.java
@@ -1,7 +1,10 @@
 package de.client.gui;
 
+import java.util.Vector;
+
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
@@ -10,16 +13,35 @@ import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 import de.client.ClientSideSettings;
+import de.shared.BO.Eigenschaft;
 
 public class ProfilEigenschaftEditor extends VerticalPanel {
 
+	private VerticalPanel vpanel = this;
 	private Button addEigenschaftenButton = new Button("+");
+	private ListBox eigenschaftenListbox = new ListBox(false);
 	
+	public ProfilEigenschaftEditor(){
+		this.add(addEigenschaftenButton);
+		
+	}
 	
 	private class AddEigenschaftenClickHandler{
-		public void onClick(ClickEvent e){
+		public void onClick(ClickEvent e) throws Exception{
+			ClientSideSettings.getEditorService().getEigenschaften(new AddEigenschaftenCallback());
+		}
+	}
+	
+	private class AddEigenschaftenCallback implements AsyncCallback <Vector<Eigenschaft>>{
+		public void onFailure(Throwable caught) {
 			
 		}
+		public void onSuccess(Vector<Eigenschaft> result) {
+			for(int i = 0; i < result.size(); i++){
+				eigenschaftenListbox.addItem(result.elementAt(i).getErlaeuterung());
+			}
+		}
+		
 	}
 	
 	
