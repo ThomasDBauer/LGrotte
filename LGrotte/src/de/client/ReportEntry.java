9ae20def6258c40d551384e12ea730d7a3f8dd23
Thomas Bauer <tb094@hdm-stuttgart.de>
2016-05-23 15:55:35 +0200

    quatsch gemacht

---
+++ LGrotte/src/de/client/ReportEntry.java
@@ -0,0 +1,13 @@
+package de.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+
+public class ReportEntry implements EntryPoint{
+
+	public void onModuleLoad() {
+		RootPanel.get().add(new Label("WOOOOOOOOOOOOOOOOOOOOOOAH"));
+	}
+
+}
