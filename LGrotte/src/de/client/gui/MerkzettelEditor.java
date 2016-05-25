3066278899e1be3076f1725dc7d399a03eaa6ec9
NicolaiEhrmanntraut <Nicolai@10.60.41.102>
2016-05-25 16:52:40 +0200

    MerzettelEditor erste Gui Elemente angelegt

--- LGrotte/src/de/client/gui/MerkzettelEditor.java
+++ LGrotte/src/de/client/gui/MerkzettelEditor.java
@@ -1,7 +1,33 @@
 package de.client.gui;
 
-public class MerkzettelEditor {
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class MerkzettelEditor extends VerticalPanel{
+	
+	private VerticalPanel vpanel = new VerticalPanel(); 
+	private FlexTable flexTable = new FlexTable();
+	private Button anzeigeButton = new Button("anzeigen");
+	private Button loeschButton = new Button("x");
+	
+	private ListBox suchprofilListBox = new ListBox();
+	
+	private Label profilLabel = new Label("ProfilName");
+	private Label ueWertLabel = new Label("in %");
+	
+	
+	
 	public MerkzettelEditor(){
+		flexTable.setWidget(0, 0, profilLabel);
+		flexTable.setWidget(0, 1, ueWertLabel);	
+		flexTable.setWidget(0, 2, anzeigeButton);
+		flexTable.setWidget(0, 3, loeschButton);
 		
+		vpanel.add(suchprofilListBox);
+		vpanel.add(flexTable);
 	}
+	
 }
