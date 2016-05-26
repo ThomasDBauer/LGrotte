0b07dd739aa9f89ed758845dda4a646fe6271962
Thomas Bauer <tb094@hdm-stuttgart.de>
2016-05-26 18:56:50 +0200

    zeug2

--- LGrotte/src/de/client/LGrotte.java
+++ LGrotte/src/de/client/LGrotte.java
@@ -3,6 +3,7 @@ package de.client;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.RootPanel;
 
+import de.client.gui.FindLove;
 import de.client.gui.LoginFrame;
 
 public class LGrotte implements EntryPoint {
