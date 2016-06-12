8ef8596a86d0146667adfcaefbfb9a1fad81c61d
enricopopaj <enrico.popaj@googlemail.com>
2016-06-12 17:58:27 +0200

    anzeigen klappt

--- LGrotte/src/de/client/gui/FremdesProfilAnzeigenEditor.java
+++ LGrotte/src/de/client/gui/FremdesProfilAnzeigenEditor.java
@@ -1,15 +1,18 @@
 package de.client.gui;
 
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class FremdesProfilAnzeigenEditor extends VerticalPanel{
 	
-		private VerticalPanel vpanel = new VerticalPanel();
+		private VerticalPanel vpanel = this;
 		private Label test = new Label("Es klappt");
+		private Button testButon = new Button("Test");
 		
 		public FremdesProfilAnzeigenEditor() {
-			vpanel.add(test);
+			this.add(test);
+			this.add(testButon);
 		}
 		
 }
