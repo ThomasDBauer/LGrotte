f96ad6a7a652c3925d5c19c235c14e8b6f7dab1a
Thomas Bauer <tb094@hdm-stuttgart.de>
2016-07-03 01:46:05 +0200

    editor interfaces kommentiert

--- LGrotte/src/de/shared/EditorService.java
+++ LGrotte/src/de/shared/EditorService.java
@@ -24,84 +24,191 @@ import de.shared.RO.ProfilEigenschaft;
  */
 @RemoteServiceRelativePath("editor")
 public interface EditorService extends RemoteService {
-
+	
+	/**
+	 * 
+	 * @see de.server.editor.EditorServiceImpl#insertProfil
+	 * (String, String, String, int, String, String, String, String, Date);
+	 */
 	void insertProfil(String email, String fname, String lname, int koerpergroesse, 
 			String geschlecht, String religion,
 			String haarfarbe, String raucher, Date geburtsdatum) 
 					throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#getProfil();
+	 */
 	Profil getProfil() throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#getAuswahlForEigenschaft(Eigenschaft);
+	 */
 	Vector<Auswahl> getAuswahlForEigenschaft(Eigenschaft e) throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#deleteSuchprofil(String);
+	 */
 	void deleteSuchprofilInfosForUser(Suchprofil sp) throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#getProfil();
+	 */
 	Profil getProfil(String email) throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#updateProfil
+	 * (String, String, int, String, String, String, String, Date, String);
+	 */
 	void updateProfil(String fname, String lname, int koerpergroesse, String geschlecht,
 			String religion, String haarfarbe, String raucher, 
 			Date geburtsdatum, String email) throws Exception;
-
+	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#deleteProfil();
+	 */
 	void deleteProfil() throws Exception;
-
+	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#insertSuchprofil
+	 * (String, String, String, String, int, int, int, int, String);
+	 */
 	void insertSuchprofil(String suchprofilname, String geschlecht, 
 			String raucher, String religion, int minAlter,
 			int maxAlter, int minGroesse, int maxGroesse, String haarfarbe) 
 					throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#updateSuchprofil
+	 * (String, int, int, String, String, String, int, int, String);
+	 */
 	void updateSuchprofil(String geschlecht, int minAlter, int maxAlter,
 			String religion, String haarfarbe, String raucher, 
 			int minGroesse, int maxGroesse, String suchprofilname) 
 					throws Exception;
-
+	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#deleteSuchprofil(String);
+	 */
 	void deleteSuchprofil(String suchprofilname) throws Exception;
 
+	/**
+	 * @see de.server.editor.EditorServiceImpl#getEigenschaften();
+	 */
 	Vector<Eigenschaft> getEigenschaften() throws Exception;
 
+	/**
+	 * @see de.server.editor.EditorServiceImpl#insertProfilInfo(Info);
+	 */
 	void insertProfilInfo(Info info) throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#insertSuchprofilInfo(Suchprofil, Info);
+	 */
 	void insertSuchprofilInfo(Suchprofil sp, Info info) throws Exception;
 
+	/**
+	 * @see de.server.editor.EditorServiceImpl#getSuchprofile();
+	 */
 	Vector<Suchprofil> getSuchprofile() throws Exception;
 
+	/**
+	 * @see de.server.editor.EditorServiceImpl#setUser(Profil);
+	 */
 	void setUser(Profil p);
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#deleteProfilInfosForUser();
+	 */
 	void deleteProfilInfosForUser() throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#getProfilesForEditor();
+	 */
 	Vector<Profil> getProfilesForEditor() throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#insertMerkzettel(String);
+	 */
 	void insertMerkzettel(Vector<String> emails) throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#deleteSuchprofil(String);
+	 */
 	void deleteMerkzettel(Vector<String> emails) throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#getMerkzettelByOwner();
+	 */
 	Vector<Merkzettel> getMerkzettelByOwner() throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#getMerkzettelByOwner();
+	 */
 	Vector<Profil> getMerkzettelProfileByOwner() throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#insertKontaktsperren(Vector);
+	 */
 	void insertKontaktsperren(Vector<String>emails) throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#insertKontaktsperre(String);
+	 */
 	void deleteKontaktsperre(Vector<String> emails) throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#getSuchprofileByName(String);
+	 */
 	Suchprofil getSuchprofileByName(String suchprofilname) throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#deleteInfo(Info);
+	 */
 	void deleteInfo(Info info) throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#deleteProfilInfo(ProfilInfo);
+	 */
 	void deleteProfilInfo(ProfilInfo pi) throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#getProfilEigenschaften();
+	 */
 	Vector<ProfilEigenschaft> getProfilEigenschaften() throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#getSuchprofilEigenschaften(String);
+	 */
 	Vector<ProfilEigenschaft> getSuchprofilEigenschaften(String spname) 
 			throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#getProfilEigenschaften(String);
+	 */
 	Vector<ProfilEigenschaft> getProfilEigenschaften(String email) throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#getKontaktsperrenByOwner();
+	 */
 	Vector<Profil> getKontaktsperrenByOwner() throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#insertBesuch(Profil);
+	 */
 	void insertBesuch(Profil besuchtesProfil) throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#insertMerkzettel(String);
+	 */
 	void insertMerkzettel(String email) throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#deleteSuchprofil(String);
+	 */
 	void insertKontaktsperre(String email) throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#deleteSuchprofilInfo(Suchprofil, Info);
+	 */
 	void deleteSuchprofilInfo(Suchprofil sp, Info info) throws Exception;
 }
--- LGrotte/src/de/shared/EditorServiceAsync.java
+++ LGrotte/src/de/shared/EditorServiceAsync.java
@@ -14,7 +14,7 @@ import de.shared.BO.Suchprofil;
 import de.shared.RO.ProfilEigenschaft;
 
 /**
- * 
+ * Das asynchrone Gegenstück des Interface @link {@link EditorService}
  * 
  * @author Thomas Bauer, Enrico Popaj und Nicolai Ehrmanntraut
  *
@@ -23,6 +23,11 @@ import de.shared.RO.ProfilEigenschaft;
 
 public interface EditorServiceAsync {
 
+	/**
+	 * 
+	 * @see de.server.editor.EditorServiceImpl#insertProfil
+	 * (String, String, String, int, String, String, String, String, Date);
+	 */
 	void insertProfil(String email, String fname, String lname,
 			int koerpergroesse, String geschlecht, String religion,
 			String haarfarbe, String raucher, Date geburtsdatum,
@@ -30,97 +35,196 @@ public interface EditorServiceAsync {
 
 	void deleteProfil(AsyncCallback callback) throws Exception;
 
+	/**
+	 * @see de.server.editor.EditorServiceImpl#getProfil();
+	 */
 	void getProfil(AsyncCallback<Profil> callback) throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#getAuswahlForEigenschaft(Eigenschaft);
+	 */
 	void getAuswahlForEigenschaft(Eigenschaft e, 
 			AsyncCallback<Vector<Auswahl>>callback) throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#getProfil();
+	 */
 	void getProfil(String email, AsyncCallback<Profil> callback) throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#deleteSuchprofil(String);
+	 */
 	void deleteSuchprofilInfosForUser(Suchprofil sp, AsyncCallback callback) 
 			throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#deleteProfilInfosForUser();
+	 */
 	void deleteProfilInfosForUser(AsyncCallback callback) throws Exception;
 
+	/**
+	 * @see de.server.editor.EditorServiceImpl#updateProfil
+	 * (String, String, int, String, String, String, String, Date, String);
+	 */
 	void updateProfil(String fname, String lname, int koerpergroesse,
 			String geschlecht, String religion, String haarfarbe,
 			String raucher, Date geburtsdatum, String email,
 			AsyncCallback callback) throws Exception;
 
+	/**
+	 * @see de.server.editor.EditorServiceImpl#insertSuchprofil
+	 * (String, String, String, String, int, int, int, int, String);
+	 */
 	void insertSuchprofil(String suchprofilname, String geschlecht,
 			String raucher, String religion, int minAlter, int maxAlter,
 			int minGroesse, int maxGroesse, String haarfarbe,
 			AsyncCallback callback) throws Exception;
 
+	/**
+	 * @see de.server.editor.EditorServiceImpl#updateSuchprofil
+	 * (String, int, int, String, String, String, int, int, String);
+	 */
 	void updateSuchprofil(String geschlecht, int minAlter, int maxAlter,
 			String religion, String haarfarbe, String raucher, 
 			int minGroesse, int maxGroesse, String suchprofilname, 
 			AsyncCallback callback) throws Exception;
 
+	/**
+	 * @see de.server.editor.EditorServiceImpl#deleteSuchprofil(String);
+	 */
 	void deleteSuchprofil(String suchprofilname, AsyncCallback callback)
 			throws Exception;
 
+	/**
+	 * @see de.server.editor.EditorServiceImpl#getEigenschaften();
+	 */
 	void getEigenschaften(AsyncCallback<Vector<Eigenschaft>> callback)
 			throws Exception;
 
+	/**
+	 * @see de.server.editor.EditorServiceImpl#insertProfilInfo(Info);
+	 */
 	void insertProfilInfo(Info info, AsyncCallback callback) throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#insertSuchprofilInfo(Suchprofil, Info);
+	 */
 	void insertSuchprofilInfo(Suchprofil sp, Info info, AsyncCallback callback) 
 			throws Exception;
 
+	/**
+	 * @see de.server.editor.EditorServiceImpl#getSuchprofile();
+	 */
 	void getSuchprofile(AsyncCallback<Vector<Suchprofil>> callback)
 			throws Exception;
 
+	/**
+	 * @see de.server.editor.EditorServiceImpl#setUser(Profil);
+	 */
 	void setUser(Profil p, AsyncCallback callback);
 
+	/**
+	 * @see de.server.editor.EditorServiceImpl#getProfilesForEditor();
+	 */
 	void getProfilesForEditor(AsyncCallback<Vector<Profil>> callback)
 			throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#insertMerkzettel(Vector);
+	 */
 	void insertMerkzettel(Vector<String> emails, AsyncCallback callback) 
 			throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#getMerkzettelByOwner();
+	 */
 	void getMerkzettelByOwner(AsyncCallback<Vector<Merkzettel>> callback) 
 			throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#getMerkzettelProfileByOwner();
+	 */
 	void getMerkzettelProfileByOwner(AsyncCallback<Vector<Profil>> callback) 
 			throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#insertKontaktsperren(Vector);
+	 */
 	void insertKontaktsperren(Vector<String>emails, AsyncCallback callback) 
 			throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#getSuchprofileByName(String);
+	 */
 	void getSuchprofileByName(String suchprofilname, AsyncCallback<Suchprofil> callback) 
 			throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#deleteInfo(Info);
+	 */
 	void deleteInfo(Info info, AsyncCallback callback) throws Exception;
 
+	/**
+	 * @see de.server.editor.EditorServiceImpl#deleteProfilInfo(ProfilInfo);
+	 */
 	void deleteProfilInfo(ProfilInfo pi, AsyncCallback callback)
 			throws Exception;
 
+	/**
+	 * @see de.server.editor.EditorServiceImpl#deleteMerkzettel(Vector);
+	 */
 	void deleteMerkzettel(Vector<String> emails, AsyncCallback callback);
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#getProfilEigenschaften();
+	 */
 	void getProfilEigenschaften(AsyncCallback<Vector<ProfilEigenschaft>>callback) 
 			throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#getSuchprofilEigenschaften(String);
+	 */
 	void getSuchprofilEigenschaften(String spname, 
 			AsyncCallback<Vector<ProfilEigenschaft>>callback) throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#getProfilEigenschaften(String);
+	 */
 	void getProfilEigenschaften(String email, 
 			AsyncCallback<Vector<ProfilEigenschaft>>callback) throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#getKontaktsperrenByOwner();
+	 */
 	void getKontaktsperrenByOwner(AsyncCallback<Vector<Profil>>callback) 
 			throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#deleteKontaktsperre(Vector);
+	 */
 	void deleteKontaktsperre(Vector<String> emails, AsyncCallback callback);
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#insertBesuch(Profil);
+	 */
 	void insertBesuch(Profil besuchtesProfil, AsyncCallback callback) 
 			throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#insertMerkzettel(String);
+	 */
 	void insertMerkzettel(String email, AsyncCallback callback) 
 			throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#insertKontaktsperre(String);
+	 */
 	void insertKontaktsperre(String email, AsyncCallback callback) 
 			throws Exception;
 	
+	/**
+	 * @see de.server.editor.EditorServiceImpl#deleteSuchprofilInfo(Suchprofil, Info);
+	 */
 	void deleteSuchprofilInfo(Suchprofil sp, Info info, AsyncCallback callback) 
 			throws Exception;
 }
