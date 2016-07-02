67dbe08d2dea0c989771443e7ea00f57bce63d11
Thomas Bauer <tb094@hdm-stuttgart.de>
2016-07-03 01:56:19 +0200

    Login kommentiert

--- LGrotte/src/de/shared/LoginService.java
+++ LGrotte/src/de/shared/LoginService.java
@@ -5,7 +5,19 @@ import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 import de.shared.BO.Profil;
 
+/**
+ * 
+ * @author Thomas Bauer
+ * 
+ * The client-side stub for the RPC service.
+ *
+ */
+
 @RemoteServiceRelativePath("login")
 public interface LoginService extends RemoteService{
+	
+	/**
+	 * @see de.server.login.LoginServiceImpl#login(String);
+	 */
 	Profil login(String requestUri) throws Exception;
 }
